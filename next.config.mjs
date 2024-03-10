2f61b30c22c2c62c095d76793f11371c9d1a3811
Gennadij Lobotskij <utyf185@mail.ru>
2024-03-10 20:31:04 +0300

    Feature: adding img config and Axios

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: "rickandmortyapi.com",
+        pathname: '/api/character/avatar/**' //звездочки, чтобы указать, что после / может что-то быть
+      }
+    ]
+  }// для того чтобы разрешить отображать img с этого ресурса
 };
 
 export default nextConfig;
